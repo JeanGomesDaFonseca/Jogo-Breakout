e66ac72745bc0d8fd77914bd962b173f30966887
Jean Gomes <jgomesfonseca21@gmail.com>
2022-06-17 14:30:17 -0300

    Construindo Tijolos

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ var game = new Phaser.Game(480, 320, Phaser.Canvas, null, {
 
     var ball;
     var paddle;
+    var bricks;
+    var newBrick;
+    var brickInfo;
 
-    function preload(){
+    function preload() {
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
-        game.stage.backgroundColor = '#D3D3D3';
+        game.stage.backgroundColor = '#eee';
         game.load.image('ball', 'img/ball.png');
         game.load.image('paddle', 'img/paddle.png');
+        game.load.image('brick', 'img/brick.png');
     }
 
-    function create(){
+    function create() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.physics.arcade.checkCollision.down = false;
         ball = game.add.sprite(game.world.width*0.5, game.world.height-25, 'ball');
@@ -24,7 +28,7 @@ var game = new Phaser.Game(480, 320, Phaser.Canvas, null, {
         ball.body.bounce.set(1);
         ball.checkWorldBounds = true;
         ball.events.onOutOfBounds.add(function(){
-            alert('Game Over!');
+            alert('Game over!');
             location.reload();
         }, this);
     
@@ -32,10 +36,39 @@ var game = new Phaser.Game(480, 320, Phaser.Canvas, null, {
         paddle.anchor.set(0.5,1);
         game.physics.enable(paddle, Phaser.Physics.ARCADE);
         paddle.body.immovable = true;
+    
+        initBricks();
     }
 
     function update(){
         game.physics.arcade.collide(ball, paddle);
         paddle.x = game.input.x || game.world.width*0.5;
+    }
 
+    function initBricks() {
+        brickInfo = {
+            width: 50,
+            height: 20,
+            count: {
+                row: 7,
+                col: 3
+            },
+            offset: {
+                top: 50,
+                left: 60
+            },
+            padding: 10
+        }
+        bricks = game.add.group();
+        for(c=0; c<brickInfo.count.col; c++) {
+            for(r=0; r<brickInfo.count.row; r++) {
+                var brickX = (r*(brickInfo.width+brickInfo.padding))+brickInfo.offset.left;
+                var brickY = (c*(brickInfo.height+brickInfo.padding))+brickInfo.offset.top;
+                newBrick = game.add.sprite(brickX, brickY, 'brick');
+                game.physics.enable(newBrick, Phaser.Physics.ARCADE);
+                newBrick.body.immovable = true;
+                newBrick.anchor.set(0.5);
+                bricks.add(newBrick);
+            }
+        }
     }
